9b27ac38ceabd29873e4fc18ff406c228d8ddba5
Steve Gargolinski <steve.gargolinski@gmail.com>
2014-05-11 13:16:59 -0400

    Initial checkin of progress project

---
+++ Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,209 @@
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Achievement
+{
+    public string Name;
+    public string Description;
+    public Texture2D IconIncomplete;
+    public Texture2D IconComplete;
+    public int RewardPoints;
+    public float TargetProgress;
+    public bool Secret;
+
+    [HideInInspector]
+    public bool Earned = false;
+    private float currentProgress = 0.0f;
+
+    public bool AddProgress(float progress)
+    {
+        if (Earned)
+        {
+            return false;
+        }
+
+        currentProgress += progress;
+        if (currentProgress >= TargetProgress)
+        {
+            Earned = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool SetProgress(float progress)
+    {
+        if (Earned)
+        {
+            return false;
+        }
+
+        currentProgress = progress;
+        if (progress >= TargetProgress)
+        {
+            Earned = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void OnGUI(Rect position, GUIStyle GUIStyleAchievementEarned, GUIStyle GUIStyleAchievementNotEarned)
+    {
+        GUIStyle style = GUIStyleAchievementNotEarned;
+        if (Earned)
+        {
+            style = GUIStyleAchievementEarned;
+        }
+
+        GUI.BeginGroup(position);
+        GUI.Box(new Rect(0.0f, 0.0f, position.width, position.height), "");
+
+        if (Earned)
+        {
+            GUI.Box(new Rect(0.0f, 0.0f, position.height, position.height), IconComplete);
+        }
+        else
+        {
+            GUI.Box(new Rect(0.0f, 0.0f, position.height, position.height), IconIncomplete);
+        }
+
+        GUI.Label(new Rect(80.0f, 5.0f, position.width - 80.0f - 50.0f, 25.0f), Name, style);
+
+        if (Secret && !Earned)
+        {
+            GUI.Label(new Rect(80.0f, 25.0f, position.width - 80.0f, 25.0f), "Description Hidden!", style);
+            GUI.Label(new Rect(position.width - 50.0f, 5.0f, 25.0f, 25.0f), "???", style);
+            GUI.Label(new Rect(position.width - 250.0f, 50.0f, 250.0f, 25.0f), "Progress Hidden!", style);
+        }
+        else
+        {
+            GUI.Label(new Rect(80.0f, 25.0f, position.width - 80.0f, 25.0f), Description, style);
+            GUI.Label(new Rect(position.width - 50.0f, 5.0f, 25.0f, 25.0f), RewardPoints.ToString(), style);
+            GUI.Label(new Rect(position.width - 250.0f, 50.0f, 250.0f, 25.0f), "Progress: [" + currentProgress.ToString("0.#") + " out of " + TargetProgress.ToString("0.#") + "]", style);
+        }
+
+        GUI.EndGroup();
+    }
+}
+
+public class AchievementManager : MonoBehaviour
+{
+    public Achievement[] Achievements;
+    public AudioClip EarnedSound;
+    public GUIStyle GUIStyleAchievementEarned;
+    public GUIStyle GUIStyleAchievementNotEarned;
+
+    private int currentRewardPoints = 0;
+    private int potentialRewardPoints = 0;
+    private Vector2 achievementScrollviewLocation = Vector2.zero;
+
+	void Start()
+	{
+	    ValidateAchievements();
+        UpdateRewardPointTotals();
+	}
+	
+    // Make sure the setup assumptions we have are met.
+    private void ValidateAchievements()
+    {
+        ArrayList usedNames = new ArrayList();
+        foreach (Achievement achievement in Achievements)
+        {
+            if (achievement.RewardPoints < 0)
+            {
+                Debug.LogError("AchievementManager::ValidateAchievements() - Achievement with negative RewardPoints! " + achievement.Name + " gives " + achievement.RewardPoints + " points!");
+            }
+
+            if (usedNames.Contains(achievement.Name))
+            {
+                Debug.LogError("AchievementManager::ValidateAchievements() - Duplicate achievement names! " + achievement.Name + " found more than once!");
+            }
+            usedNames.Add(achievement.Name);
+        }
+    }
+
+    private Achievement GetAchievementByName(string achievementName)
+    {
+        return Achievements.FirstOrDefault(achievement => achievement.Name == achievementName);
+    }
+
+    private void UpdateRewardPointTotals()
+    {
+        currentRewardPoints = 0;
+        potentialRewardPoints = 0;
+
+        foreach (Achievement achievement in Achievements)
+        {
+            if (achievement.Earned)
+            {
+                currentRewardPoints += achievement.RewardPoints;
+            }
+
+            potentialRewardPoints += achievement.RewardPoints;
+        }
+    }
+
+    private void AchievementEarned()
+    {
+        UpdateRewardPointTotals();
+        AudioSource.PlayClipAtPoint(EarnedSound, Camera.main.transform.position);        
+    }
+
+    public void AddProgressToAchievement(string achievementName, float progressAmount)
+    {
+        Achievement achievement = GetAchievementByName(achievementName);
+        if (achievement == null)
+        {
+            Debug.LogWarning("AchievementManager::AddProgressToAchievement() - Trying to add progress to an achievemnet that doesn't exist: " + achievementName);
+            return;
+        }
+
+        if (achievement.AddProgress(progressAmount))
+        {
+            AchievementEarned();
+        }
+    }
+
+    public void SetProgressToAchievement(string achievementName, float newProgress)
+    {
+        Achievement achievement = GetAchievementByName(achievementName);
+        if (achievement == null)
+        {
+            Debug.LogWarning("AchievementManager::SetProgressToAchievement() - Trying to add progress to an achievemnet that doesn't exist: " + achievementName);
+            return;
+        }
+
+        if (achievement.SetProgress(newProgress))
+        {
+            AchievementEarned();
+        }
+    }
+
+    void OnGUI()
+    {
+        float yValue = 5.0f;
+        float achievementGUIWidth = 500.0f;
+
+        GUI.Label(new Rect(200.0f, 5.0f, 200.0f, 25.0f), "-- Achievements --");
+
+        // Setup a scrollview, and then fill it with each achievement in our list.
+
+        achievementScrollviewLocation = GUI.BeginScrollView(new Rect(0.0f, 25.0f, achievementGUIWidth + 25.0f, 400.0f), achievementScrollviewLocation,
+                                                            new Rect(0.0f, 0.0f, achievementGUIWidth, Achievements.Count() * 80.0f));
+
+        foreach (Achievement achievement in Achievements)
+        {
+            Rect position = new Rect(5.0f, yValue, achievementGUIWidth, 75.0f);
+            achievement.OnGUI(position, GUIStyleAchievementEarned, GUIStyleAchievementNotEarned);
+            yValue += 80.0f;
+        }
+
+        GUI.EndScrollView();
+
+        GUI.Label(new Rect(10.0f, 440.0f, 200.0f, 25.0f), "Reward Points: [" + currentRewardPoints + " out of " + potentialRewardPoints + "]");
+    }
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour
+{
+    public AchievementManager AchievementManager;
+
+    private bool inStealthMode = false;
+    private int currentLevel = 0;
+    private int numRedPotions = 0;
+    private int numBluePotions = 0;
+    private int numGreenPotions = 0;
+    
+	void Start()
+    {
+        LevelUp();
+	}
+
+    private void LevelUp()
+    {
+        currentLevel++;
+        AchievementManager.AddProgressToAchievement("Junior Hero", 1.0f);
+        AchievementManager.AddProgressToAchievement("Experienced Hero", 1.0f);
+    }
+	
+	void Update()
+    {
+        if (inStealthMode)
+        {
+            AchievementManager.AddProgressToAchievement("The Invisible Man", Time.deltaTime);
+        }
+	}
+
+    private void PotionQuantitiesChanged()
+    {
+        AchievementManager.SetProgressToAchievement("Potion Hoarder", numRedPotions + numBluePotions + numGreenPotions);
+        if (numRedPotions > 0 && numBluePotions > 0 && numGreenPotions > 0)
+        {
+            AchievementManager.SetProgressToAchievement("Potion Collector", 1.0f);
+        }
+    }
+
+    private void BuyPotion(ref int potion)
+    {
+        potion++;
+        AchievementManager.SetProgressToAchievement("Potion Owner", 1.0f);
+        PotionQuantitiesChanged();
+    }
+
+    private void DrinkPotion(ref int potion)
+    {
+        potion--;
+        AchievementManager.AddProgressToAchievement("Town Drunk", 1.0f);
+        PotionQuantitiesChanged();
+    }
+
+    void OnGUI()
+    {
+        float xPosition = 600.0f;
+
+        GUI.Label(new Rect(xPosition, 25.0f, 150.0f, 25.0f), "Level: " + currentLevel);
+        if (GUI.Button(new Rect(xPosition + 100.0f, 25.0f, 100.0f, 25.0f), "Level Up!"))
+        {
+            LevelUp();
+        }
+
+        if (GUI.Button(new Rect(xPosition, 55.0f, 150.0f, 25.0f), "Kill a Goblin"))
+        {
+            AchievementManager.AddProgressToAchievement("Goblin Battle", 1.0f);
+        }
+
+        if (GUI.Button(new Rect(xPosition, 85.0f, 150.0f, 25.0f), "Kill the Red Dragon"))
+        {
+            AchievementManager.SetProgressToAchievement("Dragon Slayer", 1.0f);
+        }
+
+        inStealthMode = GUI.Toggle(new Rect(xPosition, 115.0f, 150.0f, 25.0f), inStealthMode, "Stealth Mode");
+
+        GUI.Label(new Rect(xPosition, 150.0f, 150.0f, 25.0f), "Red Potions: " + numRedPotions);
+        if (GUI.Button(new Rect(xPosition + 125.0f, 150.0f, 50.0f, 25.0f), "Buy") && numRedPotions < 9)
+        {
+            BuyPotion(ref numRedPotions);
+        }
+        if (GUI.Button(new Rect(xPosition + 180.0f, 150.0f, 50.0f, 25.0f), "Drink") && numRedPotions > 0)
+        {
+            DrinkPotion(ref numRedPotions);
+        }
+
+        GUI.Label(new Rect(xPosition, 180.0f, 150.0f, 25.0f), "Blue Potions: " + numBluePotions);
+        if (GUI.Button(new Rect(xPosition + 125.0f, 180.0f, 50.0f, 25.0f), "Buy") && numBluePotions < 9)
+        {
+            BuyPotion(ref numBluePotions);
+        }
+        if (GUI.Button(new Rect(xPosition + 180.0f, 180.0f, 50.0f, 25.0f), "Drink") && numBluePotions > 0)
+        {
+            DrinkPotion(ref numBluePotions);
+        }
+
+        GUI.Label(new Rect(xPosition, 210.0f, 150.0f, 25.0f), "Green Potions: " + numGreenPotions);
+        if (GUI.Button(new Rect(xPosition + 125.0f, 210.0f, 50.0f, 25.0f), "Buy") && numGreenPotions < 9)
+        {
+            BuyPotion(ref numGreenPotions);
+        }
+        if (GUI.Button(new Rect(xPosition + 180.0f, 210.0f, 50.0f, 25.0f), "Drink") && numGreenPotions > 0)
+        {
+            DrinkPotion(ref numGreenPotions);
+        }
+    }
+}
